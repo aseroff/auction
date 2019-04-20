5791642896da61d26093db52db8134c49e979d4b
Andrew Seroff <andy@seroff.co>
2019-04-20 10:52:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,11 @@ var settings = {
   "openingBid": 0,
   "firstWarning": 10,
   "secondWarning": 5,
+  "openingMessage": "/me Bidding for $auction has opened at $bid $currency. You can bid on this auction by using !bid and then the amount you want to bid. Bids must be $increment higher than the previous bid.",
+  "auctionInProgressMessage": "There is an auction for $auction already in progress!",
+  "newBidMessage": "/me @$user has the high bid at $bid $currency. Do I hear $min?",
+  "insufficientFundsMessage": "Sorry @$user, you can't afford that bid. NotLikeThis",
+  "invalidBidMessage": "Invalid bid! Minimum bid is $min $currency.",
   "firstWarningMessage": "/me Going once to $user for $bid!",
   "secondWarningMessage": "/me Going twice to $user for $bid!",
   "winningMessage": "/me Sold to $user for $bid!"
